6b5b83d2b92995ddb0ed401bce0cf9391219daf2
bxrp21 <btlawrence@mta.ca>
2016-11-17 12:01:04 -0400

    Updated Product

--- Product.java
+++
@@ -1,8 +0,0 @@
-interface Product{
-	public String getName();
-	public String getDescription();
-	public int getCost();
-	public void setName(String name);
-	public void setDescription(String description);
-	public void setCost(int cost);
-}
--- src/Product.java
+++ src/Product.java
@@ -1,4 +1,7 @@
-
-public interface Product {
-	
+	public String getName();
+	public String getDescription();
+	public int getCost();
+	public void setName(String name);
+	public void setDescription(String description);
+	public void setCost(int cost);
 }
