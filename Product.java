5c9c5b3bfd0ac82713f11488dd336952e1f5fb25
bxrp21 <btlawrence@mta.ca>
2016-11-17 11:58:49 -0400

    Product

---
+++ Product.java
@@ -0,0 +1,8 @@
+interface Product{
+	public String getName();
+	public String getDescription();
+	public int getCost();
+	public void setName(String name);
+	public void setDescription(String description);
+	public void setCost(int cost);
+}
